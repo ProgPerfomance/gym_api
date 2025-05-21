cdf67a8e64a93496817eb0bdffa4f0f18abef18a
popovevgenii <jekcatpopov@gmail.com>
2025-05-21 19:51:14 +0100

    ...

--- lib/core/headers.dart
+++ lib/core/headers.dart
@@ -1,5 +1,5 @@
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT',
   'Access-Control-Allow-Headers': '*',
 };
